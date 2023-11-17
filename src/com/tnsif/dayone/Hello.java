ef138f4d57b4b1f2e706197145da016a6714e08c
Arati Kumbar <Arati Kumbar@LAPTOP-HCPQRO39>
2023-11-17 22:05:12 +0530

    First commit

---
+++ src/com/tnsif/dayone/Hello.java
@@ -0,0 +1,17 @@
+package com.tnsif.dayone;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+                System.out.println("I");
+                System.out.println("Love");
+                System.out.println("java");
+               
+                System.out.println("programming");
+                System.out.println("Language");
+                System.out.println("Thk u");
+            
+	}
+
+}
---
+++ src/com/tnsif/dayone/IdentifiersDemo.java
@@ -0,0 +1,21 @@
+package com.tnsif.dayone;
+
+public class IdentifiersDemo {
+
+	public static void main(String[] args) {
+		
+		//invalid identifiers
+		//int class=13;//error becaiuse keyword cannot be ur identifier
+		
+		//int num 2=13;//because we use spaces in identifiers
+		
+		//valid identifiers
+		int $number=20;
+		System.out.println("value of the num variable is : "+$number);
+		
+		String studentName="Priyanka";//use camelcase
+		System.out.println("value is :"+studentName);
+
+	}
+
+}
